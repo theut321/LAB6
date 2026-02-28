4ff399b9046af4fa1c7a8c977617269064d4912c
theut321 <jairus_villafranca@dlsu.edu.ph>
2026-02-28 19:42:23 +0800

    ed

--- create.php
+++ create.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add CPU</title>
+    <link rel="stylesheet" href="style.css">
+
 </head>
 <body>
     <h2>Insert CPU Information</h2>
